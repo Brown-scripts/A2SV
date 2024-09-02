1a1a5997f947feef142b2954fa09046974129267
Brown-scripts <122943514+Brown-scripts@users.noreply.github.com>
2024-09-02 21:47:33 +0000

    Create convert_temperature.py

---
+++ convert_temperature.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def convertTemperature(self, celsius):
+        Kelvin=celsius+273.15
+        Fahrenheit=celsius*1.80+32.00
+        ans=[Kelvin,Fahrenheit]
+        return ans
+        """
+        :type celsius: float
+        :rtype: List[float]
+        """
+        
