ddd9230834bbccc6641dc5d7f7e51766a68a1e94
Brown-scripts <122943514+Brown-scripts@users.noreply.github.com>
2024-09-02 21:58:56 +0000

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        x_str = str(x)
+        return x_str == x_str[::-1]
+        
+        """
+        :type x: int
+        :rtype: bool
+        """
+        
