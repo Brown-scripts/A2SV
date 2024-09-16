a159aa70896e1fd18e5e80f6b26eeb085957af2b
Brown-scripts <122943514+Brown-scripts@users.noreply.github.com>
2024-09-16 14:45:36 +0000

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,1 +1,15 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        merged = []
+        l1, l2 = len(word1), len(word2)
 
+        for i in range(min(l1, l2)):
+            merged.append(word1[i])
+            merged.append(word2[i])
+    
+        if l1 > l2:
+            merged.extend(word1[l2:])
+        else:
+            merged.extend(word2[l1:])
+    
+        return ''.join(merged)
